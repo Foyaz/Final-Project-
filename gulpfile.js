cb111cc67713dd2a87735953694d6f5bab74d4c7
Foyaz <kayserfoyez68@gmail.com>
2018-05-16 11:57:56 -0500

    Optimized Images

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
- 
+const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+
 gulp.task('default', () =>
-    gulp.src('src/app.css')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
+    gulp.src('./src/css/style.css')
+        .pipe(autoprefixer())
         .pipe(gulp.dest('dist'))
 );
 
 gulp.task('minify-css', () => {
-    return gulp.src('styles/*.css')
-      .pipe(cleanCSS({compatibility: 'ie8'}))
-      .pipe(gulp.dest('dist'));
-  });
+    return gulp.src('./src/*.css')
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', () =>
+    gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
